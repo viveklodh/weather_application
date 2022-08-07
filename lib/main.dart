ecd1cc536daecebf15fa05fecdaf227898eafca8
vivek <viveklodh55@gmail.com>
2022-08-07 23:03:18 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:weather_application/repo.dart';
+import 'package:weather_application/weather_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
+  WeatherModel? weatherModel;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("weather app"),
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: controller,
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      weatherModel = await Repo().getWeather(controller.text);
+
+                      print(weatherModel?.main?.temp);
+
+                      setState(() {});
+                    },
+                    child: Text("search")),
+                Text("temp ${weatherModel?.main?.temp ?? "error"}"),
+                Text("max temp ${weatherModel?.main?.tempMax ?? "error"}")
+              ],
+            )));
+  }
+}
---
+++ lib/repo.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_application/weather_model.dart';
+
+class Repo {
+  getWeather(String? city) async {
+    var url =
+        "https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=43ea6baaad7663dc17637e22ee6f78f2";
+
+    final res = await http.get(Uri.parse(url));
+
+    var resBody = res.body;
+    try {
+      if (res.statusCode == 200) {
+        return WeatherModel.fromJson(json.decode(resBody));
+      }
+    } catch (e) {
+      throw Exception();
+    }
+  }
+}
